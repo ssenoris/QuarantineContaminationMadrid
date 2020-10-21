be36b6f4d38cd80dfa8f153ef4cecfd6a7a959d3
Sara Señorís Puentes <itasar.ssp@gmail.com>
2020-10-21 17:07:59 +0200

    Time series models: SARIMA and Prophet

--- libmadproject.py
+++ libmadproject.py
@@ -7,7 +7,10 @@ import matplotlib.dates as mdates
 import seaborn as sns
 import datetime as dt
 from string import ascii_letters
-
+from urllib.request import urlopen
+import json
+import plotly.express as px
+   
 
 #DATOS CALIDAD DE AIRE ANUALES: Calidad del aire. Datos diarios años 2001 a 2020, https://datos.madrid.es/portal/site/egob/menuitem.c05c1f754a33a9fbe4b2e4b284f1a5a0/?vgnextoid=aecb88a7e2b73410VgnVCM2000000c205a0aRCRD&vgnextchannel=374512b9ace9f310VgnVCM100000171f5a0aRCRD&vgnextfmt=default
 # url = 'data2020.csv'
@@ -75,7 +78,7 @@ def creo_fecha(df):
 # Label each measuring station with the district in which it's located
 def column_distrito(df):
 
-    distrito = pd.read_csv('estacion_distrito.csv', sep =';')
+    distrito = pd.read_csv('data/'+'estacion_distrito.csv', sep =';')
     dic_distrito = distrito.set_index('ESTACION')['DISTRITO'].to_dict()
         #creamos un diccionario estacion - distrito a partir de los datos del csv "distrito".
     #En el for nos recorremos df, la columna ESTACIÓN y accedemos al diccionario donde estacion es la clave y distrito es el calor
@@ -86,7 +89,7 @@ def column_distrito(df):
         distri.append(dic_distrito[i])
 
     df['distrito'] = distri
-    distrito_name = pd.read_csv('numDistrito_nombreDistrito.csv', sep =';')
+    distrito_name = pd.read_csv('data/'+'numDistrito_nombreDistrito.csv', sep =';')
     dic_distrito_name = distrito_name.set_index('num')['nombre'].to_dict()
     distri_name = []
    
@@ -133,7 +136,7 @@ def mapa_por_distritos(url):
     #with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
     #    counties = json.load(response)
 
-    file = open('distrito_geojson.geojson')
+    file = open('data/'+'distrito_geojson.geojson')
     distritos = json.load(file)
     fig = px.choropleth_mapbox(maping, geojson=distritos, locations='distrito', color='MAGNITUD',
                                 featureidkey='properties.codigoalternativo',
@@ -166,7 +169,7 @@ def mapa_por_distritos_periodos(url):
     #with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
     #    counties = json.load(response)
 
-    file = open('distrito_geojson.geojson')
+    file = open('data/'+'distrito_geojson.geojson')
     distritos = json.load(file)
     print('BEFORE QUARANTINE 2020')
 #     print('ANTES',maping_antes.FECHA.tail())
@@ -217,7 +220,7 @@ def one_column_per_element(df):
     #LOS DIFERENTES VALORES DE MAGNITUD SE DESPIEGAN si hay alguna repeticion pivot table por defecto tiene aggfunc el mean de esos valores. en este caso hay valores de magnitud en la misma fecha para distintas estaciones, hace la media. No interesaría si queremos ver cosas por distritos/zonas.
     #cambio los nombres de las columnas.
     # 1. creo un df de traduccion, 
-    df_traduccion = 'mag_translator.csv'
+    df_traduccion = 'data/'+'mag_translator.csv'
     mag_tr = pd.read_csv(df_traduccion, sep = ';')
     #2.a partir de ahi creo un diccionario numero: nombre_del_elemento 
     #2.1 se quitan tildes (en el propio df) y espacios(con la funcion lambda)
@@ -311,7 +314,7 @@ def bars_for_years(list_urls):
     plt.ylabel('Contamination (AU)')
     plt.title('Sulphur dioxide, nitrogen monoxide, nitrogen dioxide, microparticles, nitrogen oxides, ozone,\n toluene, benzene, ethylbenzene, metalxylene, paraxylene, orthoxylene contamination levels in Madrid')
     
-    
+#levels of contamination over the years
 def bars_for_years_2(list_urls):
     # 6- monóxido de carbono, 42-hidrocarburos totales, 43-metano e 44-hidrocarburos no meránicos(hexano)
     #SE EXPLUYEN: dioxido de azufre, monoxido de nitrogeno, dioxido de nitrogeno, particulas <2.5micras, partículas < 10 micras, óxidos de nitrógeno, ozono, tolueno, benceno, etilbenceno, metalxileno, paraxileno, ortoxileno (microg/m3)
@@ -364,9 +367,6 @@ def heat_map_years(list_urls):
     ax = sns.heatmap(hmap,cmap="YlGnBu",cbar_kws={'label': 'Contamination (AU)'})
     ax.set_ylabel('')
     
-
-
-
 '''FUNCIONES AEMET TIEMPO EN RETIRO Y BARAJAS'''
 def merge_json(list_json):
     dfs = []
@@ -418,13 +418,15 @@ def concat_aemet_adapt(list_json,selected = ['fecha','nombre','tmed','tmin','hor
     df = df.reset_index().rename(columns = {'fecha': 'FECHA'})
     df = serie_to_float(df)
     return df
+
 def one_column_per_element_and_distrito(df):
 
     df=df.reset_index().pivot_table(values=['Value'], index=['FECHA','distrito_name'], columns=['MAGNITUD'])
-    #LOS DIFERENTES VALORES DE MAGNITUD SE DESPIEGAN si hay alguna repeticion pivot table por defecto tiene aggfunc el mean de esos valores. en este caso hay valores de magnitud en la misma fecha para distintas estaciones, hace la media. No interesaría si queremos ver cosas por distritos/zonas.
+    #LOS DIFERENTES VALORES DE MAGNITUD SE DESPIEGAN si hay alguna repeticion pivot table por defecto tiene
+    #aggfunc el mean de esos valores. en este caso hay valores de magnitud en la misma fecha para distintas estaciones, hace la media. No interesaría si queremos ver cosas por distritos/zonas.
     #cambio los nombres de las columnas.
     # 1. creo un df de traduccion, 
-    df_traduccion = 'mag_translator.csv'
+    df_traduccion = 'data/'+'mag_translator.csv'
     mag_tr = pd.read_csv(df_traduccion, sep = ';')
     #2.a partir de ahi creo un diccionario numero: nombre_del_elemento 
     #2.1 se quitan tildes (en el propio df) y espacios(con la funcion lambda)
@@ -476,7 +478,7 @@ def contamina_and_clima(list_csv,list_json):
     'retiro_2010_2015.json']'''
 
     df1 = daily_airmad_varios(list_csv)
-    df1 = newcolumn_from_dic(df1,'mag_translator.csv', 'MAGNITUD', 'mag_nombre', 'nombre_magnitud')
+    df1 = newcolumn_from_dic(df1,'data/'+'mag_translator.csv', 'MAGNITUD', 'mag_nombre', 'nombre_magnitud')
 
 #     df2 = concat_aemet(list_json)
     df2 = concat_aemet_adapt(list_json)
@@ -513,7 +515,7 @@ def corrheat(list_csv, list_json):
 def contaminantes_barplot(url, co = True):
 #     df = daily_airmad_varios([url])
     df =daily_airmad_mu_m3(url)
-    df_traduccion = 'mag_translator.csv'
+    df_traduccion = 'data/'+'mag_translator.csv'
     mag_tr = pd.read_csv(df_traduccion, sep = ';')
     #2.a partir de ahi creo un diccionario numero: nombre_del_elemento 
     #2.1 se quitan tildes (en el propio df) y espacios(con la funcion lambda)
@@ -537,14 +539,7 @@ def contaminantes_barplot(url, co = True):
     sns.set(style="ticks")
     
     sns.set(rc={'figure.figsize':(12,6)})
-          
-#     df_traduccion = 'mag_translator.csv'
-#     mag_tr = pd.read_csv(df_traduccion, sep = ';')
-#     #2.a partir de ahi creo un diccionario numero: nombre_del_elemento 
-#     #2.1 se quitan tildes (en el propio df) y espacios(con la funcion lambda)
-#     mag_tr['mag_nombre_j'] = mag_tr['mag_nombre'].apply(lambda a: a.replace(" ","")) #quito espacios
-#     dictionary = mag_tr.set_index('MAGNITUD')['mag_nombre_j'].to_dict()
-#     labels = [dictionary[6],dictionary[9],dictionary[12],dictionary[14],]
+
     ax = sns.barplot(x='Date', y="Value", hue='MAGNITUD', data=concatenacion_dfs)
     h, l = ax.get_legend_handles_labels()
     ax.legend(h, labels, title="Contaminantes")
@@ -553,33 +548,8 @@ def contaminantes_barplot(url, co = True):
     ax.set(xticklabels=meses)
     plt.ylabel('Contaminacion media μg/m3')
     plt.xticks(rotation=45)
-    
-#     plt.axhline(y=500, color='b', linestyle='-')
     plt.show()
-# def contaminantes_barplot(url):
-# #     df = daily_airmad_varios([url])
-#     df =daily_airmad_mu_m3(url)
-#     df = df[(df.MAGNITUD == 6)|(df.MAGNITUD == 9 )|(df.MAGNITUD == 12)|(df.MAGNITUD == 14)]
-#     lista_dfs = []
-#     for name, group in df.groupby('MAGNITUD'):
-#         name = str(name)
-#         df = group.resample('M', on = 'FECHA').mean()
-#         lista_dfs.append(df)
-#     concatenacion_dfs = pd.concat(lista_dfs, axis = 0).reset_index()
-#     concatenacion_dfs["Date"] = concatenacion_dfs["FECHA"].dt.strftime('%b')
-#     sns.set(style="whitegrid")
-          
-#     df_traduccion = 'mag_translator.csv'
-#     mag_tr = pd.read_csv(df_traduccion, sep = ';')
-#     #2.a partir de ahi creo un diccionario numero: nombre_del_elemento 
-#     #2.1 se quitan tildes (en el propio df) y espacios(con la funcion lambda)
-#     mag_tr['mag_nombre_j'] = mag_tr['mag_nombre'].apply(lambda a: a.replace(" ","")) #quito espacios
-#     dictionary = mag_tr.set_index('MAGNITUD')['mag_nombre_j'].to_dict()
-#     labels = [dictionary[6],dictionary[9],dictionary[12],dictionary[14],]
-#     ax = sns.barplot(x='Date', y="Value", hue='MAGNITUD', data=concatenacion_dfs)
-#     h, l = ax.get_legend_handles_labels()
-#     ax.legend(h, labels, title="Contaminantes")
-#     plt.xticks(rotation=45)
+
 
 def daily_airmad_mu_m3(url):
     #es una función que llama a todas las anteriores
@@ -597,7 +567,7 @@ def daily_airmad_mu_m3(url):
         if valor ==6:
             df.Value[pos] = df.Value[pos]*1000
     
-    df_traduccion = 'mag_translator.csv'
+    df_traduccion = 'data/'+'mag_translator.csv'
     mag_tr = pd.read_csv(df_traduccion, sep = ';')
     #2.a partir de ahi creo un diccionario numero: nombre_del_elemento 
     #2.1 se quitan tildes (en el propio df) y espacios(con la funcion lambda)
@@ -626,12 +596,7 @@ def bars_for_years_mu_m3(list_urls,titulo):
         df2 = daily_airmad_mu_m3(url)
         
         df_magnitudes_selected = df2[(df2.MAGNITUD==1) | (df2.MAGNITUD==6)| (df2.MAGNITUD==7)| (df2.MAGNITUD==8)| (df2.MAGNITUD==9)| (df2.MAGNITUD==12)| (df2.MAGNITUD==14)]
-           #dioxido de azufre, monoxido de nitrogeno, dioxido de nitrogeno, particulas <2.5micras, partículas < 10 micras, óxidos de nitrógeno, ozono, tolueno, benceno, etilbenceno, metalxileno, paraxileno, ortoxileno (microg/m3)
 
-    #se excluyen = 6- monóxido de carbono, 42-hidrocarburos totales, 43-metano e 44-hidrocarburos 
-#     no meránicos(hexano)
-        # print(df_magnitudes_selected.head(16))
-#         test2 = df_magnitudes_selected.resample('SMS', on = 'FECHA').sum().reset_index()[['FECHA', 'Value']]
         
         test2 = df_magnitudes_selected.resample('M', on = 'FECHA').sum().reset_index()[['FECHA', 'Value']]
         print('------------------------------------------------------------------')
@@ -674,7 +639,7 @@ def contamina_mu_m3_and_clima(list_csv,list_json):
     print('list_csv:', list_csv)
     df1 = daily_airmad_varios_mu_m3(list_csv)
     print('primero',df1)
-    df1 = newcolumn_from_dic(df1,'mag_translator.csv', 'MAGNITUD', 'mag_nombre', 'nombre_magnitud')
+    df1 = newcolumn_from_dic(df1,'data/'+'mag_translator.csv', 'MAGNITUD', 'mag_nombre', 'nombre_magnitud')
     print('segundo',df1)
 
     df2 = concat_aemet(list_json)
@@ -725,7 +690,7 @@ def mapa_por_distritos_periodos(url):
     #with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
     #    counties = json.load(response)
 
-    file = open('distrito_geojson.geojson')
+    file = open('data/'+'distrito_geojson.geojson')
     distritos = json.load(file)
     print('BEFORE QUARANTINE 2020')
 #     print('ANTES',maping_antes.FECHA.tail())
@@ -779,16 +744,14 @@ def mapa_por_distritos_periodos_varios(list_url):
     print('Distrito con maxima caida es CHAMARTÍN', contamina.set_index('distritoantes').porcentaje_antes_despues.idxmin())
     print('--------------------------------------')
 
-    from urllib.request import urlopen
-    import json
-    import plotly.express as px
+ 
     #with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
     #    counties = json.load(response)
 
-    file = open('distrito_geojson.geojson')
+    file = open('data/'+'distrito_geojson.geojson')
     distritos = json.load(file)
     
-    print("\033[;36m"+'From 15th March to 30th April'+ '\033[4;35m'+ ' 2019'+'\033[0;m') 
+    print("\033[;36"+'From 15th March to 30th April'+ '\033[4;35m'+ ' 2019'+'\033[0;m') 
 #     print('ANTES',maping_antes.FECHA.tail())
     fig = px.choropleth_mapbox(maping_antes, geojson=distritos, locations='distrito', color='Value',range_color=(0, 130),
                                 featureidkey='properties.codigoalternativo',
@@ -801,7 +764,7 @@ def mapa_por_distritos_periodos_varios(list_url):
     
     
 #     print('From 15th March to 30th April'+ "\033[4;35;47m"+ '2020'+'\033[0;m') 
-    print("\033[;36m"+'From 15th March to 30th April'+ '\033[4;35m' + ' 2020'+'\033[0;m') 
+    print("\033[;36"+'From 15th March to 30th April'+ '\033[4;35m' + ' 2020'+'\033[0;m') 
 #     print('DESPUÉS',maping_despues.FECHA)
     fig = px.choropleth_mapbox(maping_despues, geojson=distritos, locations='distrito', color='Value',range_color=(0, 130),
                                 featureidkey='properties.codigoalternativo',
@@ -841,38 +804,7 @@ def media_period_quarantine(list_url, list_mag= [1,6,7,8,9,12,14]):
     m['year'] = year
 #     m['year'] = pd.to_datetime(m['year'])
     m['value_year'] = value_year
-#     sns.set(style="whitegrid")
 
-#     ax = sns.barplot(x='year', y="value_year", data=m)
-#     h, l = ax.get_legend_handles_labels()
-#     ax.legend(h,year, title="Quarantine")
-#     plt.xticks(rotation=90)
-#     plt.ylabel('Contamination during quarantine period')
-# -------------------------------------------------------------
-    
-#     import plotly.graph_objs as go
-#     # create trace1 
-#     trace1 = go.Bar(
-#                     x = m.year,
-#                     y = m.value_year,
-#                     name = "Contamination during quarantine period 2",
-#                     marker = dict(color = 'rgba(255, 174, 255, 0.5)',
-#                                  line=dict(color='rgb(0,0,0)',width=1.5)))
-#     data = [trace1]
-# #     layout = go.Layout(barmode = "group")
-# #     layout = {
-# #   'xaxis': {'title': 'Years'},
-# #   'barmode': 'relative',
-# #   'title': 'Contamination during quarantine period 2'
-# # };
-#     layout = dict(title = 'Contamination during quarantine period',
-#               xaxis= dict(title= 'Year',ticklen= 5,zeroline= False),
-#               yaxis= dict(title= 'Contamination level',ticklen= 5,zeroline= False)
-#              )
-    
-
-#     fig = go.Figure(data = data, layout = layout)
-#     iplot(fig)
 # -------------------------------------------------------------
     import plotly.express as px
     data = px.data.gapminder()
@@ -887,11 +819,9 @@ def media_period_quarantine(list_url, list_mag= [1,6,7,8,9,12,14]):
         'xanchor': 'center',
         'yanchor': 'top'})
     fig.update_xaxes(tickangle=-90)
-    
-#     plt.xticks(rotation=-90)
-#     plt.xticks(range(len(year)), year)
+
     fig.show()
-#     return [data.year,m.year]
+
 
 def corrheat_triangular(list_csv, list_json):
     sns.set(style="white")
@@ -924,26 +854,18 @@ def mapa_por_distritos_madridcentral(list_url):
     
     mask_antes = (df_magnitudes_selected.FECHA >= '2017-11-30')&(df_magnitudes_selected.FECHA < '2018-11-30')
     mask_despue = (df_magnitudes_selected.FECHA >= '2018-11-30')&(df_magnitudes_selected.FECHA < '2019-11-30')
-    
-    
-#     maping_antes =df_magnitudes_selected[mask_antes].groupby(['FECHA','MAGNITUD', 'distrito']).mean().reset_index()  
-#     maping_despues =df_magnitudes_selected[mask_despue].groupby(['FECHA','MAGNITUD', 'distrito']).mean().reset_index()
+
     maping_antes =df_magnitudes_selected[mask_antes].groupby(['distrito']).mean().reset_index()  
     maping_despues =df_magnitudes_selected[mask_despue].groupby(['distrito']).mean().reset_index()  
-#     print('maping:', maping)
+
     
     contamina = pd.merge(left = maping_antes[['distrito','Value']],right =maping_despues[['distrito','Value']],left_index = True,right_index = True,how = 'outer',suffixes = ('antes','despues'))
     contamina['porcentaje_antes_despues'] = contamina['Valuedespues']*100/contamina['Valueantes']
-#     print(contamina)
+
     print('DATOS MADRID CENTRAL PARA MONÓXIDO DE CARBONO Y ÓXIDOS DE NITRÓGENO')
     
-    
-#     print('mean, porcentaje',contamina['porcentaje_antes_despues'].describe())
-#     print('idxmax distrito con minima caida', contamina.set_index('distritoantes').porcentaje_antes_despues.idxmax())
-#     print('idxmax distrito con maxima caida', contamina.set_index('distritoantes').porcentaje_antes_despues.idxmin())
-    
     print('--------------------------------------')
-    print("\x1b[1;33m"+'% Contaminación respecto al año anterior',contamina['porcentaje_antes_despues'].describe())
+    print("\x1b[1;33"+'% Contaminación respecto al año anterior',contamina['porcentaje_antes_despues'].describe())
     print('--------------------------------------')
     print('Distrito con minima caida es RETIRO', contamina.set_index('distritoantes').porcentaje_antes_despues.idxmax())
     print('--------------------------------------')
@@ -951,15 +873,10 @@ def mapa_por_distritos_madridcentral(list_url):
     print('--------------------------------------')
     
     
-    
-
-    from urllib.request import urlopen
-    import json
-    import plotly.express as px
     #with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
     #    counties = json.load(response)
 
-    file = open('distrito_geojson.geojson')
+    file = open('data/'+'distrito_geojson.geojson')
     distritos = json.load(file)
     print('--------------------------------------')
     print('BEFORE MADRID CENTRAL')
@@ -987,7 +904,7 @@ def mapa_por_distritos_madridcentral(list_url):
                             )
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     fig.show()
-    print("\033[;36m"+ 'La caída en distrito centro es inferior al 5%')
+    print("\033[;36"+ 'La caída en distrito centro es inferior al 5%')
 
 #ahora hay diferentes opciones de presentación del df
 #OPCIÓN1:
